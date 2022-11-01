8950727e2afa9a644a39ef2c5daf99fcdb95fe29
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-01 14:41:37 +0530

    Add files via upload
    Added UC2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UC2LastUser
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+          UC2LastUser pattern = new UC2LastUser();
+            Console.WriteLine(pattern.validate_Last("Joy"));
+
+        }
+}
+}
---
+++ UC2LastUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC2LastUser
+{
+    internal class UC2LastUser
+    {
+        public static string Lastdigit = "^[A-Z][A-za-z]{3}$";
+        public bool validate_Last(string Lastname)
+        {
+            return Regex.IsMatch(Lastname, Lastdigit);
+        }
+    }
+}
+
