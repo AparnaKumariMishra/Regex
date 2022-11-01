632f06da157740448cfa4eaf887121ef8d008bbb
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-01 14:42:29 +0530

    Add files via upload
    Added UC3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using UserCase3;
+
+Console.WriteLine("Enter the Email");
+string Email = Console.ReadLine();
+if(UC3Email.validate(Email))
+{
+    Console.WriteLine("valid Email");
+}
+else
+{
+    Console.WriteLine("invalid Email");
+}
---
+++ UC3Email.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserCase3
+{
+    internal class UC3Email
+    {
+        public static string Email = "^[A-Z]{3}[.][a-z]{3}@[a-z]{2}[.][a-z]{2}$";//YAS.raj@co.in
+ 
+        public static bool validate(string Word )
+        {
+            return Regex.IsMatch(Word, Email);
+        }
+
+    }
+}
