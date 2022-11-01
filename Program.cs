4fa350b42f5b8c323f78563a7f748fabec466496
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-01 14:43:21 +0530

    Add files via upload
    Added UC4

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using UserCase4;
+
+Console.WriteLine("Enter the MobileNumber");
+string Number = Console.ReadLine();
+if (UC4MobileNo.validate(Number))
+{
+    Console.WriteLine("valid Number");
+}
+else
+{
+    Console.WriteLine("invalid Number");
+}
---
+++ UC4MobileNo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserCase4
+{
+    internal class UC4MobileNo
+    {
+        public static string MobileNo = "^[9][1][\\s]?[0-9]{10}$";
+        public static bool validate(string Number)
+        {
+            return Regex.IsMatch(Number, MobileNo);
+        }
+    }
+}
