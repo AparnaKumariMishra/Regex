8c0cecc2cb2d80e0a124e67a44603f7559d7be10
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-01 14:40:44 +0530

    Add files via upload
    Added UC1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex1
+{
+   public class program
+    {
+        public static void Main(string[] args)
+        {
+             UC1FirstUser pattern = new UC1FirstUser();
+            Console.WriteLine(pattern.validate_First("Try"));
+            //Console.WriteLine("Please Enter Name");
+            //string pattern = Console.ReadLine();
+            //UC1FirstUser obj = new UC1FirstUser();
+            //bool val = obj.ValidateFirstName(pattern);
+            //if (val)
+              //  Console.WriteLine("Pattern Matched");
+            //else
+             //   Console.WriteLine("Pattern Not Matched");
+            //Console.WriteLine("---------------");
+
+        }
+    }
+}
---
+++ UC1FirstUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex1
+{
+    public class UC1FirstUser
+    {
+          public static string Firstdigit = "^[A-Z]{1}[a-z]{2}$";
+        public bool validate_First(string Firstname)
+        {
+          return Regex.IsMatch(Firstname, Firstdigit);
+        }
+        //public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-z]{3}$";
+        //public bool ValidateFIRSTNAME(string firstname)
+        //{
+          //  return Regex.IsMatch(firstname, REGEX_FIRSTNAME);
+        //}
+    }
+}
