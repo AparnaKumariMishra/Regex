055975655833e83714b40ce411b25d948cd0ee05
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-11-01 14:44:25 +0530

    Add files via upload
    Added UC5

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using UserCase5;
+
+Console.WriteLine("Enter the Password");
+string password = Console.ReadLine();
+if (UC5.validate(password))
+{
+    Console.WriteLine("valid password");
+}
+else
+{
+    Console.WriteLine("please enter valid password");
+}
---
+++ UC5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserCase5
+{
+    internal class UC5
+    {
+        public static string password = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+        public static bool validate(string value)
+        {
+            return Regex.IsMatch(value, password);
+        }
+    }
+}
